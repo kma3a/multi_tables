5fe9f15fb8fcbe90913a8419ab848466de913dfe
Kelsey Ando <kma3a@virginia.edu>
2015-03-18 13:40:59 -0400

    added third test and it passes

--- multi_spec.js
+++ multi_spec.js
@@ -10,6 +10,13 @@ describe('Multi Table', function() {
 		var expected = [[1,2],[2,4]];
 		expect(multiTable(2)).toEqual(expected);
 	})
+
+
+	it("if 5 will give the ones and twos", function() {
+		var expected = [[1,2,3,4,5],[2,4,6,8,10],[3,6,9,12,15],[4,8,12,16,20],[5,10,15,20,25]];
+		expect(multiTable(5)).toEqual(expected);
+	})
+
 })
 
 
