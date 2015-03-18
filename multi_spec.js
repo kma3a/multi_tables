3ad7aac6bc428ba824103dff883b5e220e83839d
Kelsey Ando <kma3a@virginia.edu>
2015-03-18 13:44:43 -0400

    adding test to test to throw error if you don't put in a number

--- multi_spec.js
+++ multi_spec.js
@@ -6,15 +6,19 @@ describe('Multi Table', function() {
 		expect(multiTable(1)).toEqual([[1]]);
 	})
 	
-	it("if 2 will give the ones and twos", function() {
+	it("2 will give the ones and twos", function() {
 		var expected = [[1,2],[2,4]];
 		expect(multiTable(2)).toEqual(expected);
 	})
 
 
-	it("if 5 will give the ones and twos", function() {
+	it("5 will give the ones and twos", function() {
 		var expected = [[1,2,3,4,5],[2,4,6,8,10],[3,6,9,12,15],[4,8,12,16,20],[5,10,15,20,25]];
 		expect(multiTable(5)).toEqual(expected);
+	})
+	
+	it("will throw error if you don't pass in a number", function(){
+		expect(multiTable("banana")).toThrow(new Error("Please input a number"));
 	})
 
 })
