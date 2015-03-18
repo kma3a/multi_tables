994d803fb04c4343c786ace1a21d31119dacf697
Kelsey Ando <kma3a@virginia.edu>
2015-03-18 13:32:03 -0400

    correcting the first test and adding a second test

--- multi_spec.js
+++ multi_spec.js
@@ -3,7 +3,12 @@ var multiTable = require('./multi.js');
 describe('Multi Table', function() {	
 
 	it("1 to be 1", function() {
-		expect(multiTable(1)).toEqual("1");
+		expect(multiTable(1)).toEqual(1);
+	})
+	
+	it("if 2 will give the ones and twos", function() {
+		var expected = [[1,2],[2,4]];
+		expect(multiTable(2)).toEqual(expected);
 	})
 })
 
